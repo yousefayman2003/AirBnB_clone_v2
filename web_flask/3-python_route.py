9f66ebc1ac1edc4573e7ac03cfa23049783ca7d2
yousefayman <yousefayman20003@gmail.com>
2024-02-16 03:33:34 -0800

    upload first 5 flask tasks

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Starts a Flask web application at 0.0.0.0:5000"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays c with parameter"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """displays python with parameter"""
+    return 'Python ' + (text.replace('_', ' ') if text else 'is cool')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts a Flask web application at 0.0.0.0:5000"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """displays Hello HBNB!"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """displays c with parameter"""
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python(text):
+    """displays python with parameter"""
+    return 'Python ' + (text.replace('_', ' ') if text else 'is cool')
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """displays a number"""
+    return '{:d} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
