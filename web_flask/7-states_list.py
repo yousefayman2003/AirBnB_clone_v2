1e039bce17bbc0ef7cb653cba831b461fd12226d
yousefayman <yousefayman20003@gmail.com>
2024-02-16 08:51:25 -0800

    fix bug

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
-
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_session(exception):
-    """remove the current SQLAlchemy Session"""
+def dispose(exception):
+    """Removes current storage session """
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """displays states listed in alphabetical order"""
-    states = list(storage.all(State).values())
-    return render_template('7-states_list.html', states=states)
+@app.route('/states_list')
+def states():
+    """ Display list of all the states in sorted order"""
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
